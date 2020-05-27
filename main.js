47976986f065101ad0cba9961b639b39d48134fc
Kaan Yilmaz <douscriptist@gmail.com>
2020-05-28 00:40:26 +0300

    init image-resizer

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		title: 'Image Resizer',
+		width: 500,
+		height: 600,
+	});
+
+	// mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+	mainWindow.loadFile(`${__dirname}/app/index.html`);
+}
+
+app.on('ready', createMainWindow);
