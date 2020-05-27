a5ef71322774fcbdabd701e453b72539f1b30e91
Kaan Yilmaz <douscriptist@gmail.com>
2020-05-28 02:32:58 +0300

    into to menu's & etc

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const isDevMode = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
+let aboutWindows;
 
 function createMainWindow() {
 	mainWindow = new BrowserWindow({
@@ -22,34 +23,81 @@ function createMainWindow() {
 	mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
+function createAboutWindow() {
+	aboutWindow = new BrowserWindow({
+		title: 'About Image Resizer',
+		width: 200,
+		height: 200,
+		icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+		resizable: false,
+		backgroundColor: '#e6aa07',
+	});
+
+	aboutWindow.loadFile(`${__dirname}/app/about.html`);
+}
+
 app.on('ready', () => {
 	createMainWindow();
 
 	const mainMenu = Menu.buildFromTemplate(menu);
 	Menu.setApplicationMenu(mainMenu);
 
-	globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
-	globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
-		mainWindow.toggleDevTools()
-	);
+	// globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
+	// globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
+	// 	mainWindow.toggleDevTools()
+	// );
 
 	mainWindow.on('closed', () => (mainWindow = null));
 });
 
 const menu = [
 	// For mac file option
-	...(isMac ? [{ role: 'appMenu' }] : []),
-	{
-		label: 'File',
-		submenu: [
-			{
-				label: 'Quit',
-				// accelerator: isMac ? ' Command+W' : 'Ctrl+W',
-				accelerator: 'CmdOrCtrl+W', // Cross platform
-				click: () => app.quit(),
-			},
-		],
-	},
+	...(isMac
+		? [
+				{
+					// For file menu on the top bar just for MAC
+					// role: 'appMenu'
+
+					// Or custom just for MAC
+					// For windows need to do another tab etc
+					label: app.name,
+					submenu: [
+						{
+							label: 'About',
+							click: createAboutWindow,
+						},
+					],
+				},
+		  ]
+		: []),
+	{ role: 'fileMenu' },
+	...(isDevMode
+		? [
+				{
+					label: 'Development',
+					submenu: [
+						{ role: 'reload' },
+						{ role: 'forcereload' },
+						{ type: 'separator' },
+						{ role: 'toggledevtools' },
+					],
+				},
+		  ]
+		: []),
+	// For windows help & about menu in different tab
+	...(!isMac
+		? [
+				{
+					role: 'Help',
+					submenu: [
+						{
+							label: 'About',
+							click: createAboutWindow,
+						},
+					],
+				},
+		  ]
+		: []),
 ];
 
 app.on('window-all-closed', () => {
